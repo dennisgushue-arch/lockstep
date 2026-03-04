a0036635dff53a3c00925ae302514c37869b867e
dennisgushue-arch <dennis.gushue@gmail.com>
2026-03-04 21:30:31 +0000

    Redirect /capture to landing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,7 @@ import Missed from "@/pages/missed";
 import TestIntentPage from "@/pages/test-intent";
 import DebugPage from "@/pages/debug";
 import CreditsPage from "@/pages/credits";
-import { Switch, Route } from "wouter";
+import { Switch, Route, Redirect } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -14,7 +14,6 @@ import NotFound from "@/pages/not-found";
 import Landing from "@/pages/landing";
 import AuthPage from "@/pages/auth";
 import Dashboard from "@/pages/dashboard";
-import CapturePage from "@/pages/capture";
 import ReflectionPage from "@/pages/reflection";
 import LockInPage from "@/pages/lock-in";
 import StakesPage from "@/pages/stakes";
@@ -37,7 +36,9 @@ function Router() {
         <Route path="/" component={Landing} />
         <Route path="/auth" component={AuthPage} />
         <Route path="/dashboard" component={Dashboard} />
-        <Route path="/capture" component={CapturePage} />
+        <Route path="/capture">
+          <Redirect to="/" />
+        </Route>
         <Route path="/detection" component={DetectionPage} />
         <Route path="/reflection" component={ReflectionPage} />
         <Route path="/lock-in" component={LockInPage} />
